1237856f927a223ba9c630b1bc21dac06e304429
Minwoo <jungmonster@naver.com>
2016-12-11 20:47:27 +0900

    02 Mouse Over Event

---
+++ Assets/Scripts/UI_Component/UI_MouseOverEvent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+
+public class UI_MouseOverEvent : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public GameObject objSubMenu;
+    public Button btnMenu;
+
+    void Awake()
+    {
+        objSubMenu.SetActive(false);
+        btnMenu.onClick.AddListener(ToggleSubMenu);
+    }
+
+    void ToggleSubMenu()
+    {
+        objSubMenu.SetActive(!objSubMenu.activeSelf);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        objSubMenu.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        objSubMenu.SetActive(false);
+    }
+}
