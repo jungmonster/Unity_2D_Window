fa40d60c5be2fa2dacaa14e2645626515acc06c9
DESKTOP-P8GSDDD\jungm <icdi0116@naver.com>
2016-12-12 16:30:44 +0900

    ...

---
+++ Assets/Scripts/DesignPattern/DP_Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DP_Singleton <T> : MonoBehaviour where T : MonoBehaviour {
+
+    protected static T instance = null;
+    public static T GetIstance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType(typeof(T)) as T;
+
+                if(instance == null)
+                {
+                    GameObject obj = new GameObject("SingletonObject");
+                    instance = obj.AddComponent(typeof(T)) as T;
+                }
+            }
+
+            return instance;
+        }
+    }
+}
